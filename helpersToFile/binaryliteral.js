7ca00e1caefee62f23c4f8c4ecb31b0fbcc8fc9f
becket <becket.bowes@gmail.com>
2022-03-18 22:57:45 -0400

    initial commit

---
+++ helpersToFile/binaryliteral.js
@@ -0,0 +1,14 @@
+// module.export = {
+//     binaryLiteral: 
+// }
+
+const text = "i've seen starbeams on the shoulder of Onprion"
+
+const binaryLiteral = function(text) {
+    text = text.split()
+    text.forEach(element.charCodeAt.toString(2));
+    console.log(text);
+    return text;
+};
+
+console.log(binaryLiteral(text));
