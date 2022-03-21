21f8b080643909be8e7bd734dfe124a02ce2ae76
becket <becket.bowes@gmail.com>
2022-03-20 22:13:55 -0400

    all posts showing

--- controller/viewroutes.js
+++ controller/viewroutes.js
@@ -1,16 +1,20 @@
 const sequelize = require('./connection');
-const { Literal, Reader, Comment, Neat } = require('../models');
+const { Literal, Reader, Comment } = require('../models');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
     Literal.findAll({
+        order: [[ 'createdAt', 'DESC' ]],
         attributes: ['id', 'title', 'image', 'imageAlt', 'keywords', 'article', 'createdAt',
                 [ sequelize.literal('(SELECT COUNT(*) FROM neat WHERE literal.id = neat.literalKey)'), 'ohNeat' ]],
         include: [
                 { model: Comment, attributes: ['id', 'text', 'createdAt',], include: { model: Literal, attributes: ['title'] }},
                 { model: Reader, attributes: ['user']}]    
         })
-    .then(data => { res.render('home', data[0].get({ plain: true })); })
+    .then(data => { 
+        const article = data.map(article => article.get({ plain: true }));
+        res.render('home', { article }); 
+    })
     .catch(err => { console.log(err); res.status(500).json(err); });
 });
 
